a35c2ae0c5db649378da6501810b3f7c427310ce
Cyan <Cyan@git.com>
2023-09-13 22:59:15 +0800

    update np method

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
+import numpy as np
 import torch
 
 # 使用torch.Tensor()创建一个空的张量
-list = [1,2,3,4,5]
-print(list[1:-1])
+list = np.empty(0)
+print(list)
--- text_classification/text2tensor.py
+++ text_classification/text2tensor.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from transformers import AutoTokenizer
 from datasets import load_dataset
@@ -21,8 +22,11 @@ def convert_to_tensors(example):
 transformed_train_dataset = train_dataset.map(convert_to_tensors)
 transformed_test_dataset = test_dataset.map(convert_to_tensors)
 
-dataset = {'train': {'text': [], 'label': []}, 'test': {'text': [], 'label': []}}
+dataset = {'train': {'text': np.empty(0), 'label': np.empty(0)}, 'test': {'text': np.empty(0), 'label': np.empty(0)}}
 
-list = transformed_test_dataset[0]["input_ids"]
-print(list)
-print(type(list))
+transformed_text = np.array(transformed_test_dataset[0]["input_ids"])
+transformed_text = transformed_text[0][1:-1]
+print(transformed_text)
+print(type(transformed_text))
+print(transformed_text.shape)
+dataset['test']['text'].append(transformed_text)
