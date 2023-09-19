4f814e935c737cea56f37233c49ad23b1e8ac641
Cyan <Cyan@git.com>
2023-09-19 23:13:05 +0800

    commit lstm

--- test.py
+++ test.py
@@ -1,8 +1,19 @@
-import torch
-from torch import nn
+# import torch
+# from torch import nn
+#
+# pool = nn.AvgPool1d(kernel_size=4)
+# y = torch.randn(64, 379, 32)
+# z = pool(y)
+#
+# print(z.shape)
+import jieba
+import numpy as np
 
-pool = nn.AvgPool1d(kernel_size=4)
-y = torch.randn(64, 379, 32)
-z = pool(y)
-
-print(z.shape)
+# l1 = [1, 2, 3, 4]
+# print(l1)
+#
+# np.save('l1', l1)
+data = np.load('text_classification/agnews_number_dataset.npz')
+print(type(data))
+print(type(data['train_text']))
+print(data['train_text'].shape)
--- text_classification/dataset_download.py
+++ text_classification/dataset_download.py
@@ -1,4 +1,5 @@
 # 使用huggingface下载数据集
+import numpy as np
 from datasets import load_dataset
 from torch.utils.data import DataLoader
 
@@ -17,7 +18,7 @@ print(f"训练数据集的长度为: {train_data_size}")
 print(f"测试数据集的长度为: {test_data_size}")
 
 # 定义批处理大小和工作进程数
-batch_size = 4
+batch_size = 1
 
 # 创建训练集和测试集的DataLoader
 train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
@@ -26,13 +27,26 @@ test_dataloader = DataLoader(test_dataset, batch_size=batch_size)
 # # 显示train_dataloader中的第一份数据
 # print(next(iter(train_dataloader)))
 
+train_sentence_length = []
+test_sentence_length = []
+
 for data in train_dataloader:
-    print(type(data))
-    print(data)
+    # print(type(data))
+    # print(data)
+
     text = data['text']
-    label = data['label']
-    print(text)
-    print(label)
-    print(len(text))
-    print(len(label))
-    break
+    # print(text)
+    # print(len(text[0].split()))
+    train_sentence_length.append(len(text[0].split()))
+
+    # label = data['label']
+    # print(label)
+    # print(len(label))
+
+
+for data in test_dataloader:
+    text = data['text']
+    test_sentence_length.append(len(text[0].split()))
+
+np.save('train_sentence_length', train_sentence_length)
+np.save('test_sentence_length', test_sentence_length)
